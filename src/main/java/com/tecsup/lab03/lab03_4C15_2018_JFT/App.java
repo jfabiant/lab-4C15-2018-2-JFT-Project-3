7033d2c2147aadc362a9ee5b6936bda4e5413a68
jfabiant <rronaldo_jhon_20@hotmail.com>
2018-09-13 08:47:57 -0500

    First Commit

---
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2018_JFT/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab03_4C15_2018_JFT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
